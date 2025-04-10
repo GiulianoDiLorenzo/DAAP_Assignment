45cfb2194a438a6cd8da443c997257d1e3755938
GiulianoDiLorenzo <giuliano.dilorenzo@mail.polimi.it>
2025-04-10 20:24:15 +0200

    Update encoder.m
    Hanning windowing and 50% overlapping (COLA condition) and plots section

--- functions/encoder.m
+++ functions/encoder.m
@@ -8,14 +8,13 @@
 
 %% Define parameters
 % Apply a pre-emphasis HPF (freqz(b,1) to plot transfer function)
-b = [1, -0.975];    % denominator coefficients
+b = [1, -0.975];        % denominator coefficients
 s = filter(b, 1, s);
 
-% Hamming window (length=256, hop=256)
-M = 256;        % segment length (windows)
-win_len = M;
-hop_size = M;
-win = hamming(win_len);
+% Hanning windowing (window length 256 samples, 50% overlap)
+win_len = 256;      
+hop_size = win_len/2;
+win = hanning(win_len);
 n_frames = floor((length(s) - win_len)/hop_size) + 1;
 
 % LPC model parameters
@@ -23,13 +22,12 @@ lpc_coeffs = zeros(n_frames, 10);
 pitch_periods = zeros(n_frames, 1);
 gains = zeros(n_frames, 1);
 frame_errors = zeros(n_frames, win_len);
-% pred_gain = zeros(n_frames, 1);
 
 % Voiced vs unvoiced frames
 [is_voiced, ~] = voicedframedetection(s, win, hop_size);
 
 % Custom plots
-plot_idx = [42, 80];
+plot_idx = [42, 82];
 
 %% Estimate LPC coefficients
 disp("================================");
@@ -54,15 +52,11 @@ for n = 1 : n_frames
     % A(z) coefficients using Levinson-Durbin recursion
     [A, ~, ~] = levinson(r, p);
 
-    % ----------- PEZZOLI -----------
-    % Alternative solution use lpc
-    % a(ii, 1:orderLPC+1) = lpc(sn.*win, orderLPC);
-    % ----------- PEZZOLI -----------
-
     % Take care of NaNs (because of r=0 for silent frames)
     if any(isnan(A))
         % LPC coefficients not updated
         A = zeros(1, p);
+
     else
         % LPC coefficients, excluding 1
         lpc_coeffs(n, 1:p) = -A(2:end);
@@ -90,75 +84,91 @@ for n = 1 : n_frames
         end
     end
     
-    % Plots for spectra, error in time and frequency
-    if ismember(n, plot_idx)
+    % ------------------ PLOTS SECTION ------------------
+    if plot_bool
+        if ismember(n, plot_idx)
+            % Plots for spectra, error in time and frequency - FIGURE "N"
+            [H, w] = freqz(1, A);   % shaping filter and w axis
+            S = fft(frame);         % frame spectrum
+            f = (0:win_len-1)*(fs/win_len);     % frequency axis (up to fs)
+    
+            figure(n)
+            sgtitle("File " + filename + " - Frame n." + n + " (p = " + p +")")
+    
+            % Spectra comparison
+            subplot(3,1,1)
+            plot(f(1:win_len/2), db(abs(S(1:win_len/2))))
+            hold on
+            plot(w./(2*pi) * fs, db(abs(H)))
+            hold off
+            title("Original spectrum $S$ and LPC approximation $H$")
+            xlabel("$f$ [Hz]")
+            ylabel("$|S_n|$, $|H|$ [dB]")
+            grid on
+            xlim([0 fs/2])
+            legend("Original spectrum", "Shaping filter approximation")
+    
+            % Error in time
+            t = (0 : 1/fs : 1/fs*(win_len-1))*10e3;   % [ms] time axis
+            subplot(3,1,2)
+            plot(t, frame_errors(n, :))
+            title("Prediction error - time domain")
+            xlabel("$t$ [ms]")
+            ylabel("$e$")
+            grid on
+            xlim([min(t) max(t)])
+    
+            % Error in frequency
+            E = fft(frame_errors(n, :));    % error spectrum
+            subplot(3,1,3)
+            plot(f(1:win_len/2), db(abs(E(1:win_len/2))))
+            title("Prediction error - frequency domain")
+            xlabel("$f$ [Hz]")
+            ylabel("$|E|$ [dB]")
+            grid on
+            xlim([0 fs/2])
         
-        [H, w] = freqz(1, A);   % shaping filter and w axis
-        S = fft(frame);         % frame spectrum
-        f = (0:M-1)*(fs/M);     % frequency axis (up to fs)
-
-        figure()
-        sgtitle("File " + filename + " - Frame n." + n + " (p = " + p +")")
-
-        % Spectra comparison
-        subplot(3,1,1)
-        plot(f(1:M/2), db(abs(S(1:M/2))))
-        hold on
-        plot(w./(2*pi) * fs, db(abs(H)))
-        hold off
-        title("Original spectrum $S$ and LPC approximation $H$")
-        xlabel("$f$ [Hz]")
-        ylabel("$|S_n|$, $|H|$ [dB]")
-        grid on
-        xlim([0 fs/2])
-        legend("Original spectrum", "Shaping filter approximation")
-
-        % Error in time
-        t = (0 : 1/fs : 1/fs*(M-1))*10e3;   % [ms] time axis
-        subplot(3,1,2)
-        plot(t, frame_errors(n, :))
-        title("Prediction error - time domain")
-        xlabel("$t$ [ms]")
-        ylabel("$e$")
-        grid on
-        xlim([min(t) max(t)])
-
-        % Error in frequency
-        E = fft(frame_errors(n, :));    % error spectrum
-        subplot(3,1,3)
-        plot(f(1:M/2), db(abs(E(1:M/2))))
-        title("Prediction error - frequency domain")
-        xlabel("$f$ [Hz]")
-        ylabel("$|E|$ [dB]")
-        grid on
-        xlim([0 fs/2])
-        
+            % Plots time frame and spectra - FIGURE N+1
+            figure(n+1)
+            sgtitle("File " + filename + " - Frame n." + n)
+    
+            % Frame in time
+            subplot(2, 2, 1)
+            plot(t, frame)
+            title("Original frame - time domain")
+            xlabel("$t$ [ms]")
+            ylabel("$s_n$")
+            grid on
+            xlim([min(t) max(t)])
+    
+            % Frame in frequency
+            subplot(2, 2, 2)
+            plot(f(1:win_len/2), db(abs(S(1:win_len/2))))
+            title("Original frame - frequency domain")
+            xlabel("$f$ [Hz]")
+            ylabel("$|S_n|$ [dB]")
+            grid on
+            xlim([0 fs/2])
+    
+            % Plots prediction error and excitation (decoder) - FIGURE N+2
+            figure(n+2)
+            sgtitle("File " + filename + " - Frame n." + n)
+    
+            % Error in time
+            t = (0 : 1/fs : 1/fs*(win_len-1))*10e3;   % [ms] time axis
+            subplot(2,1,1)
+            plot(t, frame_errors(n, :))
+            title("Prediction error - time domain")
+            xlabel("$t$ [ms]")
+            ylabel("$e$")
+            grid on
+            xlim([min(t) max(t)])
+        end
     end
-
-    % Plots time frame and spectra
-    if ismember(n, plot_idx)
-        figure()
-        sgtitle("File " + filename + " - Frame n." + n)
-
-        % Frame in time
-        subplot(2, 1, 1)
-        plot(t, frame)
-        xlabel("$t$ [ms]")
-        ylabel("$s_n$")
-        grid on
-        xlim([min(t) max(t)])
-
-        % Frame in frequency
-        subplot(2, 1, 2)
-        plot(f(1:M/2), db(abs(S(1:M/2))))
-        xlabel("$f$ [Hz]")
-        ylabel("$|S_n|$ [dB]")
-        grid on
-        xlim([0 fs/2])
-    end
+    % ------------------ PLOTS SECTION ------------------
 end
 
 %% Save encoded data
-save("lpc10_encoding.mat", 'fs', 'M', 'n_frames', 'lpc_coeffs', 'gains', 'pitch_periods', 'is_voiced');
+save("lpc10_encoding.mat", 'fs', 'win_len', 'hop_size', 'n_frames', 'lpc_coeffs', 'gains', 'pitch_periods', 'is_voiced');
 disp("Encoding complete");
 disp("================================");
